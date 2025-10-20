c5b99b94cfa8e4799cd37b3d8d801f3437d0ea81
Yuke Li <liyuke@shanghaitech.edu.cn>
2025-10-21 00:40:14 +0800

    update metrics.py

--- metrics.py
+++ metrics.py
@@ -26,7 +26,7 @@ def readImages(renders_dir, gt_dir):
     renders = []
     gts = []
     image_names = []
-    for fname in os.listdir(renders_dir):
+    for fname in tqdm(os.listdir(renders_dir), desc="Load images"):
         render = Image.open(renders_dir / fname)
         gt = Image.open(gt_dir / fname)
         renders.append(tf.to_tensor(render).unsqueeze(0)[:, :3, :, :])
@@ -70,9 +70,13 @@ def evaluate(model_paths):
                 lpipss = []
 
                 for idx in tqdm(range(len(renders)), desc="Metric evaluation progress"):
-                    ssims.append(ssim(renders[idx].cuda(), gts[idx].cuda()))
-                    psnrs.append(psnr(renders[idx].cuda(), gts[idx].cuda()))
-                    lpipss.append(lpips(renders[idx].cuda(), gts[idx].cuda(), net_type='vgg'))
+                    renders[idx] = renders[idx].to(device)
+                    gts[idx] = gts[idx].to(device)
+                    ssims.append(ssim(renders[idx], gts[idx]))
+                    psnrs.append(psnr(renders[idx], gts[idx]))
+                    lpipss.append(lpips(renders[idx], gts[idx], net_type='vgg'))
+                    renders[idx] = renders[idx].to('cpu')
+                    gts[idx] = gts[idx].to('cpu')
 
                 print("  SSIM : {:>12.7f}".format(torch.tensor(ssims).mean(), ".5"))
                 print("  PSNR : {:>12.7f}".format(torch.tensor(psnrs).mean(), ".5"))
